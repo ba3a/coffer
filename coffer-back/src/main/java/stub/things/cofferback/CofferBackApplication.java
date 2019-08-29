9910697e062fc90c24ceae55748c3c1a06775d62
ba3a <petrovvd@gmail.com>
2019-08-29 18:07:46 +0300

    Initial commit

---
+++ coffer-back/src/main/java/stub/things/cofferback/CofferBackApplication.java
@@ -0,0 +1,13 @@
+package stub.things.cofferback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofferBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofferBackApplication.class, args);
+	}
+
+}
